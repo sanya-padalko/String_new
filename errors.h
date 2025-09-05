4856f711616036e9abec32903074c121ce709f72
sanya-padalko <apadalko90@gmail.com>
2025-09-05 14:48:33 +0300

    Create errors.h

---
+++ errors.h
@@ -0,0 +1,16 @@
+#ifndef _ERRORS_H_
+#define _ERRORS_H_
+
+#define my_assert(comp, code_err) if (comp) {print_err_message(__FILE__, __FUNCTION__, __LINE__, code_err);}
+
+enum ERRORS {
+    NOTHING        = 3,
+    NAN_ERR        = 1,
+    NULLPTR        = 0,
+    INFINITY_DIGIT = 2
+};
+
+void print_err_message(const char *file_name, const char* func_name, const int n_line,
+                       const int error_code);
+
+#endif // _ERRORS_H
